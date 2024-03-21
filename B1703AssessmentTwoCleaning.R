f4573c188fcfc320d4bcce9e81fdce28211bc3df
seanmccr <seanmccrone1@gmail.com>
2024-03-21 20:53:14 +0000

    21.03.2024 Update

---
+++ B1703AssessmentTwoCleaning.R
@@ -0,0 +1,20 @@
+# -----  B1703 Pre-Tableau Cleaning | 21.03.2024 -----]
+
+ATP2023Correct <- readxl::read_xlsx("/Users/seanmccrone/Desktop/MASTERS DEGREE/Course Material/B1703/Assessment 2/ATP2023CorrectMatches.xlsx")
+
+ATP2023Incorrect <- read.csv("/Users/seanmccrone/Desktop/MASTERS DEGREE/Course Material/B1703/Assessment 2/ATP2023SeasonIncorrectMatches.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
