275fac78905d0c16f2e6b38f8a8cee62dc6f1096
seanmccr <seanmccrone1@gmail.com>
2024-03-22 00:05:21 +0000

    22.03.2024 Update
    Pre-clean code

--- B1703AssessmentTwoCleaning.R
+++ B1703AssessmentTwoCleaning.R
@@ -1,20 +1,30 @@
 # -----  B1703 Pre-Tableau Cleaning | 21.03.2024 -----]
 
 ATP2023Correct <- readxl::read_xlsx("/Users/seanmccrone/Desktop/MASTERS DEGREE/Course Material/B1703/Assessment 2/ATP2023CorrectMatches.xlsx")
-
 ATP2023Incorrect <- read.csv("/Users/seanmccrone/Desktop/MASTERS DEGREE/Course Material/B1703/Assessment 2/ATP2023SeasonIncorrectMatches.csv")
 
+# Load the dplyr package for data manipulation
+library(dplyr)
+library(lubridate)
 
+# Exclude columns 29 to 36 from ATP2023Correct
+ATP2023Correct <- select(ATP2023Correct, -c(29:36))
+ATP2023Incorrect <- select(ATP2023Incorrect, -c())
 
+# Assuming the date column in ATP2023Incorrect is named 'Date'
+# Convert the date from 'YYYYMMDD' to 'YYYY-MM-DD'
+ATP2023Incorrect$tourney_date <- ymd(ATP2023Incorrect$tourney_date)
 
-
-
-
-
-
-
-
-
+# Renaming variables
+ATP2023Incorrect <- ATP2023Incorrect %>%
+  rename(
+    W1stIn = w_1stIn,
+    W1stWon = w_1stWon,
+    W2ndWon = w_2ndWon,
+    L1stIn = l_1stIn,
+    L1stWon = l_1stWon,
+    L2ndWon = l_2ndWon
+  )
 
 
 
